32807f9b2e1ccea6e061bbcb7fa05d6e0665ab5a
vaibhavwaghmode581-eng <vaibhavwaghmode581@gmail.com>
2026-06-04 10:52:09 +0530

    fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import PortfolioPage from "./portfolio/PortfolioPage";
 import Dashboard from "./dashboard/Dashboard";
+import PortfolioPage from "./portfolio/PortfolioPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Dashboard />} />
-
-        <Route
-          path="/user/:username"
-          element={<PortfolioPage />}
-        />
-
+        <Route path="/user/:username" element={<PortfolioPage />} />
       </Routes>
     </BrowserRouter>
   );
